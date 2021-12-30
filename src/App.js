6b75de4d0fff0ed0761f7b9024379647222a2b9f
Mithu-12 <mithuvowmick96@gmail.com>
2021-12-30 18:49:41 +0800

    last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar/Navbar';
+import Slidebar from './Slidebar/Slidebar';
+import Dashboard from './Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+                <Navbar/>
+                <div class="container-fluid" id="main">
+                 <div class="row row-offcanvas row-offcanvas-left">
+                   <Slidebar/>
+                  <Dashboard/>
+                
+             </div>
+            </div>  
+        </div>  
+        );
 }
 
 export default App;
